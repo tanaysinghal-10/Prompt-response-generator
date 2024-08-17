2b57d4f02e0fc152a253e56f4aeeba6b85804fb7
tanaysinghal-10 <tanaysinghal6@gmail.com>
2024-08-17 12:41:17 +0530

    Remove unwanted comments

--- src/main/java/com/ai/example/AIController.java
+++ src/main/java/com/ai/example/AIController.java
@@ -37,14 +37,4 @@ public class AIController {
         return response;
     }
 
-//    @GetMapping("/ai/generate")
-//    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-//        return Map.of("generation", chatModel.call(message));
-//    }
-//
-//    @GetMapping("/ai/generateStream")
-//    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-//        Prompt prompt = new Prompt(new UserMessage(message));
-//        return chatModel.stream(prompt);
-//    }
 }
