68c5ad396999fec81fd07aaed00f4d254b0c7aff
tanaysinghal-10 <tanaysinghal6@gmail.com>
2024-08-18 08:57:27 +0530

    Added few more cases

--- src/main/java/com/ai/example/AIController.java
+++ src/main/java/com/ai/example/AIController.java
@@ -22,18 +22,45 @@ public class AIController {
         this.chatModel = chatModel;
     }
 
+    // For getting complete output at once
     @GetMapping("/staticPrompt")
-    public String getPromptResponse(@RequestParam String prompt) {
+    public String getPromptResponse1(@RequestParam String prompt) {
 
         String response = chatModel.call(prompt);
         return response;
     }
 
+    // For getting output in chunks (Flux of String) just like ChatGpt
+    @GetMapping("/staticPromptFlux")
+    public Flux<String> getPromptResponse2(@RequestParam String prompt) {
+
+        Flux<String> response = chatModel.stream(prompt);
+        return response;
+    }
+
+    // For getting a JSON response instead of Text
+    @GetMapping("/staticPromptJSON")
+    public Flux<ChatResponse> getPromptResponse3(@RequestParam String prompt) {
+
+        Prompt promptActual = new Prompt(prompt);
+        Flux<ChatResponse> response = chatModel.stream(promptActual);
+        return response;
+    }
+
+
 
     @PostMapping(value = "/dynamicPrompt", consumes = "text/plain")
-    public String receiveString(@RequestBody String input) {
+    public String receiveString1(@RequestBody String input) {
 
         String response = chatModel.call(input);
+        return response;
+    }
+
+
+    @PostMapping(value = "/dynamicPromptFlux", consumes = "text/plain")
+    public Flux<String> receiveString2(@RequestBody String input) {
+
+        Flux<String> response = chatModel.stream(input);
         return response;
     }
 
